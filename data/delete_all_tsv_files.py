1beaf3a165badf01a33bb2d3bef1edc70436c99f
Oscar Khaing <akhaing@ucsd.edu>
2024-11-25 00:10:00 -0800

    slight reorganization of test files and setup scripts, deleted/modified defunct files

---
+++ data/delete_all_tsv_files.py
@@ -0,0 +1,9 @@
+# deletes all tsv files in PIMCO-TEXT2SQL/data/
+# this should be run in PIMCO-Text2SQL folder
+import os
+directory = os.getcwd()
+for filename in os.listdir(directory):
+    file_path = os.path.join(directory, filename)
+    if filename.endswith(".tsv") and os.path.isfile(file_path):
+        os.remove(file_path)
+        print(f"Deleted: {file_path}")
